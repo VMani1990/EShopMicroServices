8a8732f83c2164ebdebfa951cb351e30b0112485
Manikanta Vangara <manivangara90@outlook.com>
2025-08-16 13:03:38 +0530

    New implementation

---
+++ src/Services/Catalog/Catalog_API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog_API.Models;
+using MediatR;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Catalog_API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommndHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create Product entity from command object
+            //save to database
+            //return CreateProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description=command.Description,
+                ImageFile=command.ImageFile,
+                Price=command.price
+            };
+            //save to database
+            //retunr result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
